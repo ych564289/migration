5963d2a3e8314a4505e3749f88dd7a97fe0a45e2
兔叽 <1835528513@qq.com>
2026-02-24 14:13:30 +0800

    Instrument 文件下载

--- src/main/java/com/example/migration/dao/master/mapper/SpInstrumentBalanceClosingAsAtMapper.java
+++ src/main/java/com/example/migration/dao/master/mapper/SpInstrumentBalanceClosingAsAtMapper.java
@@ -1,12 +1,13 @@
 package com.example.migration.dao.master.mapper;
 
-import com.example.migration.dao.master.entity.SpCashBalanceClosingAsAt;
 import com.example.migration.dao.master.entity.SpInstrumentBalanceClosingAsAt;
 import com.example.migration.pojo.export.req.CashExportReq;
 
 import java.util.List;
 
-public interface SpCashBalanceClosingAsAtMapper {
+public interface SpInstrumentBalanceClosingAsAtMapper {
 
-    List<SpCashBalanceClosingAsAt> querySpCashBalanceClosingAsAt(CashExportReq req);
+    List<SpInstrumentBalanceClosingAsAt> querySpInstrumentBalanceClosingAsAt(CashExportReq req);
+
+
 }
