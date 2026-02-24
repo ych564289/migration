d77f48b2b6c6187f0e98fbd5c29d0adbe746ad6a
brooke.li <brooke.li@gtjas.com.hk>
2026-02-24 14:20:08 +0800

    Instrument

--- src/main/java/com/example/migration/dao/master/entity/SpInstrumentBalanceClosingAsAt.java
+++ src/main/java/com/example/migration/dao/master/entity/SpInstrumentBalanceClosingAsAt.java
@@ -2,9 +2,23 @@ package com.example.migration.dao.master.entity;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class SpInstrumentBalanceClosingAsAt {
 
+    private String clntCode;
 
+    private String acctType;
+
+    private String market;
+
+    private String tradingCcy;
+
+    private String instrument;
+
+    private String balanceType;
+
+    private BigDecimal asAt;
 
 }
