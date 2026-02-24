6a672cbc450bec1d8cceff7ad40a67b2b357d52f
兔叽 <1835528513@qq.com>
2026-02-24 17:54:23 +0800

    Instrument 文件下载

--- src/main/resources/text/中转脚本.sql
+++ src/main/resources/text/中转脚本.sql
@@ -1,25 +1,5 @@
-elect o.clntcode, case
-	when cms.defaulttradingacc = 'CASH' then '1'
-	when cms.defaulttradingacc = 'MRGN' then '2'
-	when cms.defaulttradingacc = 'DVP' then '3'
-	when cms.defaulttradingacc = 'SBL' then '4'
-	when cms.defaulttradingacc = 'SBLLender' then '5'
-END as accounts,
-tb.TTLMarketID ,
-o.instrument,
-sum(o.asat) ledger  from "REVIEW801LEDGERSTKv5" o, CMS_VIEW cms , TTLMarketBoard tb
-where
-    o.market = tb.Market and
-    o.ClntCode = cms.accountcode
-group by
-o.clntcode, case
-	when cms.defaulttradingacc = 'CASH' then '1'
-	when cms.defaulttradingacc = 'MRGN' then '2'
-	when cms.defaulttradingacc = 'DVP' then '3'
-	when cms.defaulttradingacc = 'SBL' then '4'
-	when cms.defaulttradingacc = 'SBLLender' then '5'
-END   ,
-tb.TTLMarketID ,
-o.instrument
+SELECT CINACTIVEBUY, ctodaybuy, CTODAYCONFIRMBUY, ctodaysell,	CTODAYCONFIRMSELL , * FROM VCBACCOUNT
 
-into new table  "4.1FINAL_CASHelo_ONHAND"
+
+SELECT  trim( CLIENTID) as CLIENTID , ttl.tradingaccseq , ttl.marketid , ttl.instrumentid,   TINACTIVEBUY , TINACTIVESELL ,
+        TTODAYBUY , TTODAYSELL , TTODAYCONFIRMBUY , TTODAYCONFIRMSELL , * FROM VCBTRADINGACC ttl
