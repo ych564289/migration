656dd0a71313568c535d713fc1d3824be6eba388
兔叽 <1835528513@qq.com>
2026-02-25 09:10:09 +0800

    Instrument 文件下载

--- src/main/resources/text/中转脚本.sql
+++ src/main/resources/text/中转脚本.sql
@@ -3,3 +3,17 @@ SELECT CINACTIVEBUY, ctodaybuy, CTODAYCONFIRMBUY, ctodaysell,	CTODAYCONFIRMSELL 
 
 SELECT  trim( CLIENTID) as CLIENTID , ttl.tradingaccseq , ttl.marketid , ttl.instrumentid,   TINACTIVEBUY , TINACTIVESELL ,
         TTODAYBUY , TTODAYSELL , TTODAYCONFIRMBUY , TTODAYCONFIRMSELL , * FROM VCBTRADINGACC ttl
+
+---Stock---
+
+select SUBSTRING( SUBACCOUNTID , 0 ,7), InstrumentID , BS, SUM(convert(DECIMAL(16,8), InstLedgerDelta ))
+from TTLMQOrders where SubAccountID  like '%068927%' and MQdatetime >= '2024-12-21 15:00:00.000' and convert(DECIMAL(16,8), InstLedgerDelta) <> 0 and MQstatus <> 'Fail'
+                   and Instrumentid = trim('00939') group by BS, subaccountid, instrumentid
+
+----- Cash ----
+
+select SubAccountID , SUM(convert(DECIMAL(16,8), CashLedgerDelta )) from   TTLMQOrders where SubAccountID like '%212291%'
+                                                                                         and MQdatetime >= '2024-07-31 19:00:00.000' and MQstatus <> 'Fail' and convert(DECIMAL(16,8), CashLedgerDelta) <> 0 group by SubAccountID
+
+
+
