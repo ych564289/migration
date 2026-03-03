9058de02064caf57f8dc5be7e37effe5a74fe617
兔叽 <1835528513@qq.com>
2026-03-03 14:30:26 +0800

    日志迁移

--- src/main/java/com/example/migration/service/InstrumentVersionService.java
+++ src/main/java/com/example/migration/service/InstrumentVersionService.java
@@ -3,5 +3,5 @@ package com.example.migration.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.migration.dao.master.entity.InstrumentVersion;
 
-public interface InstrumentVersionService {
+public interface InstrumentVersionService extends IService<InstrumentVersion> {
 }
